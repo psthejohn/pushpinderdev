feaf35cb075d855bd3d62d607724e9db6a85b79f
Pushpinder Singh <psthejohn@Pushpinders-MacBook-Air.local>
2025-10-23 12:23:25 +0530

    updated awards & Recognitions

--- src/app/testimonials/certificate/certificate.component.ts
+++ src/app/testimonials/certificate/certificate.component.ts
@@ -8,17 +8,43 @@ import { Component, HostListener } from '@angular/core';
 export class CertificateComponent {
   // --- Certificates data ---
   certificates = [
-    { image: '../../../assets/Certificates/JAVA SPRINGBOOT CERTIFIED FULL STACK DEVELOPER NIIT STACKROUTE.jpeg', title: 'CERTIFIED FULL STACK (NIIT STACKROUTE)' },
-    { image: '../../../assets/Certificates/scrum_fundamentals.png', title: 'SCRUM FUNDAMENTALS CERTIFIED' },
-    { image: '../../../assets/Certificates/gen_ai_fundamentals.png', title: 'GEN AI FUNDAMENTALS' },
-    { image: '../../../assets/Certificates/ai_for_all.png', title: 'AI FOR ALL' },
-    { image: '../../../assets/Certificates/application_security.png', title: 'APPLICATION SECURITY' },
+    {
+      image:
+        '../../../assets/Certificates/JAVA SPRINGBOOT CERTIFIED FULL STACK DEVELOPER NIIT STACKROUTE.jpeg',
+      title: 'CERTIFIED FULL STACK (NIIT STACKROUTE)',
+    },
+    {
+      image: '../../../assets/Certificates/scrum_fundamentals.png',
+      title: 'SCRUM FUNDAMENTALS CERTIFIED',
+    },
+    {
+      image: '../../../assets/Certificates/gen_ai_fundamentals.png',
+      title: 'GEN AI FUNDAMENTALS',
+    },
+    {
+      image: '../../../assets/Certificates/ai_for_all.png',
+      title: 'AI FOR ALL',
+    },
+    {
+      image: '../../../assets/Certificates/application_security.png',
+      title: 'APPLICATION SECURITY',
+    },
     // Add more if needed
   ];
 
   // Pagination for certificates
   certPage = 1;
   certPerPage = 4;
+  // Ledger modal state
+  showLedgerModal = false;
+
+  openLedgerModal() {
+    this.showLedgerModal = true;
+  }
+
+  closeLedgerModal() {
+    this.showLedgerModal = false;
+  }
 
   get paginatedCertificates() {
     const start = (this.certPage - 1) * this.certPerPage;
@@ -26,7 +52,8 @@ export class CertificateComponent {
   }
 
   nextCertPage() {
-    if (this.certPage * this.certPerPage < this.certificates.length) this.certPage++;
+    if (this.certPage * this.certPerPage < this.certificates.length)
+      this.certPage++;
   }
 
   prevCertPage() {
@@ -35,11 +62,31 @@ export class CertificateComponent {
 
   // --- Awards data ---
   awards = [
-    { image: '../../../assets/Awards & Recognitions/Pinnacle Award_ Pushpinder Singh_15_02_2023.png', title: 'PINNACLE AWARD 2023' },
-    { image: '../../../assets/Awards & Recognitions/Pinnacle Award_Pushpinder Singh_19_07_2024.png', title: 'PINNACLE AWARD 2024' },
-    { image: '../../../assets/Awards & Recognitions/CSR_EYE_CAMP_26_02_2024.png', title: 'EYE CAMP 2024' },
-    { image: '../../../assets/Awards & Recognitions/Falcon_Award_team_testimonial_26_08_2025.png', title: 'FALCON AWARD 2025' },
-    { image: '../../../assets/Awards & Recognitions/Pinnacle_award_Testimonial_Pushpinder_Singh_19_07_2024.png', title: 'PINNACLE AWARD TESTIMONIAL' },
+    {
+      image:
+        '../../../assets/Awards & Recognitions/Pinnacle Award_ Pushpinder Singh_15_02_2023.png',
+      title: 'PINNACLE AWARD 2023',
+    },
+    {
+      image:
+        '../../../assets/Awards & Recognitions/Pinnacle Award_Pushpinder Singh_19_07_2024.png',
+      title: 'PINNACLE AWARD 2024',
+    },
+    {
+      image:
+        '../../../assets/Awards & Recognitions/CSR_EYE_CAMP_26_02_2024.png',
+      title: 'EYE CAMP 2024',
+    },
+    {
+      image:
+        '../../../assets/Awards & Recognitions/Falcon_Award_team_testimonial_26_08_2025.png',
+      title: 'FALCON AWARD 2025',
+    },
+    {
+      image:
+        '../../../assets/Awards & Recognitions/Pinnacle_award_Testimonial_Pushpinder_Singh_19_07_2024.png',
+      title: 'PINNACLE AWARD TESTIMONIAL',
+    },
     // Add more if needed
   ];
 
@@ -53,7 +100,8 @@ export class CertificateComponent {
   }
 
   nextAwardPage() {
-    if (this.awardPage * this.awardPerPage < this.awards.length) this.awardPage++;
+    if (this.awardPage * this.awardPerPage < this.awards.length)
+      this.awardPage++;
   }
 
   prevAwardPage() {
